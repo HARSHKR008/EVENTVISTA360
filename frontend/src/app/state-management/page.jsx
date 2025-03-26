9f3bfdc677ee102e8a13184e6b40a01bb75c2965
HARSHKR008 <i.harshk0008@gmail.com>
2025-03-26 11:42:06 +0530

    Create page.jsx

---
+++ frontend/src/app/state-management/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StateManagement = () => {
+  return (
+    <div>
+        <h1 className='text-center font-bold text-3xl my-6'>Event handling</h1>
+    </div>
+  )
+}
+
+export default StateManagement
