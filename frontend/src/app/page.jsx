4728cb303e4d0edf0606973cbd63d3818a7ed0f2
PrashantS09 <i.prashantsingh007@gmail.com>
2025-05-14 11:26:47 +0530

    front end changes and logo added

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -5,38 +5,42 @@ const HeroSection = () => {
     return (
         <div>
             {/* Hero Area */}
-            <section className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: "url('/pexels-rosario-fernandes-26325-3835638.jpg')" }}>
-                <div className="absolute inset-0 bg-black/50 flex flex-col justify-center items-center text-center text-white px-">
+            <section className="relative h-screen bg-cover bg-center" style={{ backgroundImage: "url('/pexels-rosario-fernandes-26325-3835638.jpg')", backgroundAttachment: 'fixed' }}>
+            <div className='bg-black/50 h-full pb-20'>
 
-                    <div className="mb-8">
+                <div className=" h-full container mx-auto grid grid-cols-12 text-left text-white">
+
+                    <div className="my-auto col-span-7">
                         {/* Replace with your actual logo */}
-                        <img src="/" alt="Logo" className="mx-auto w-24 mb-2" />
-                        <h1 class="text-6xl font-extrabold text-white mb-6 font-roboto">
-          EVENT VISTA 360
-        </h1>
-        <h2 class="text-2xl italic font-light mb-6 text-gray-100 font-roboto">
-          Where Every Detail Tells Your Story.
-        </h2>
+                        <img src="/logo2.png" alt="Logo" className="w-84 h-84 mb-4" />
+                        <h1 class="text-6xl font-extrabold text-amber-100 mb-6 font-roboto">
+                            EVENT VISTA 360
+                        </h1>
+                        <h2 class="text-2xl italic font-light mb-6 text-amber-100 font-roboto">
+                            Where Every Detail Tells Your Story.
+                        </h2>
                     </div>
-
-                    <div className='flex flex-col md:flex-row items-center justify-around w-full gap-8'>
-                        <div>
-                            <img src="" alt="" width={500}/>
-                        </div>
+                    <div className='mt-auto col-span-5'>
                         <div className=''>
-                            <h2 className="text-4xl md:text-6xl font-bold leading-tight mb-4">Where Fairytales <br /> Become Reality</h2>
-                            <p className="text-lg md:text-xl mb-6">Your trusted partner in wedding planning.</p>
-                            <Link 
-                                href="/browse" 
-                                className="inline-block bg-[#e8d5c0] text-gray-800 font-semibold py-2 px-6 rounded border border-gray-700 hover:bg-[#d4c3af] transition"
-                            >
-                                BROWSE
-                            </Link>
+                            <div>
+                                <img src="" alt="" width={500} />
+                            </div>
+                            <div className=''>
+                                <h2 className="text-4xl md:text-6xl font-bold leading-tight mb-4">Where Fairytales <br /> Become Reality</h2>
+                                <p className="text-lg md:text-xl mb-6">Your trusted partner in wedding planning.</p>
+                                <Link
+                                    href="/browse"
+                                    className="inline-block bg-[#e8d5c0] text-gray-800 font-semibold py-2 px-6 rounded border border-gray-700 hover:bg-[#d4c3af] transition"
+                                >
+                                    BROWSE
+                                </Link>
+                            </div>
                         </div>
                     </div>
 
 
                 </div>
+            </div>
             </section>
 
             {/* Info Section */}
